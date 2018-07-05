71b74a075ebfbef9e98ee2944bc1b744d4799ae0
Sunil Kumar Sangaraju <sangarajusunil93@gmail.com>
2018-07-05 12:06:12 -0500

    Alert Assignment

--- udemyangular/src/app/app.module.ts
+++ udemyangular/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { warningComponent } from './Warning/warning.component';
+import { SuccessComponent } from './success/success.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    warningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule
