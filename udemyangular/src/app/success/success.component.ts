f894671589d4d0e71c1c941b0ce27105d62fc481
Sunil Kumar Sangaraju <sangarajusunil93@gmail.com>
2018-07-06 10:42:20 -0500

    sample

---
+++ udemyangular/src/app/success/success.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success',
+  templateUrl: './success.component.html',
+  styleUrls: ['./success.component.css']
+})
+export class SuccessComponent implements OnInit {
+
+  userName = "";
+  buttonStatus;
+
+   constructor() { 
+    this.buttonStatus = false;
+  }
+
+  onCheckingStatus(){
+    if (this.userName.length>0 ) {
+      this.buttonStatus = true;
+    }
+    return this.buttonStatus;
+  }
+
+  resetButton(){
+
+    if (this.buttonStatus.valid) {
+      this.buttonStatus.resetButton();
+    }
+
+  }
+
+  ngOnInit() {
+  }
+
+}
