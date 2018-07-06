c37c93bb240fbc7576cb2ea1f17dd8c2b0baf0fe
Sunil Kumar Sangaraju <sangarajusunil93@gmail.com>
2018-07-06 10:41:40 -0500

    Two way binding - Assignment2

--- udemyangular/src/app/app.module.ts
+++ udemyangular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { warningComponent } from './Warning/warning.component';
@@ -13,7 +14,8 @@ import { SuccessComponent } from './success/success.component';
     SuccessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
